654d735f5c7db30ce29e91d7e0d278deb5e551f2
cbastn <pro2j.lol@gmail.com>
2019-11-17 23:13:43 -0700

    CodeWars kata Practice

---
+++ Desktop/CodeWars/8kyu/Multiply 17-11-19/Multiply.js
@@ -0,0 +1,15 @@
+/*
+The code does not execute properly. Try to figure out why.
+function multiply(a, b){
+    a * b
+}*/
+
+function multiply(a, b){
+   return a * b
+  }
+
+result = multiply(2,3);
+
+console.log(result);
+
+
